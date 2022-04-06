dddd2e052f41665adb020c44aa6f9aa2ab6acbe4
AdulIt <spilberg.max@bk.ru>
2022-04-06 23:33:15 +0600

    refactor: new imgs & icons

---
+++ src/components/LogIn.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="login">
+        <form class="login__form">
+            <img src="../icons/logo.svg" alt="logo">
+            <div class="btns">
+                <button class="btn-modal">Регистрация</button>
+                <button class="btn-modal active">Вход</button>
+            </div>
+
+             
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
